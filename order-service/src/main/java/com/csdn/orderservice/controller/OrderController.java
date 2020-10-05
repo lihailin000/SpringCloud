712f8f666020a92136cbbd31336dc089952536aa
lihailin000 <40458962+lihailin000@users.noreply.github.com>
2020-10-05 20:02:42 +0800

    order-service提交

---
+++ order-service/src/main/java/com/csdn/orderservice/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.csdn.orderservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @GetMapping("query")
+    public String query(){
+        return "list order";
+    }
+}
